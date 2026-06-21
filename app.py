482bd41bfe254b6188702917098db96b7b27784f
sableanmol6-wq <sableanmol6@gmail.com>
2026-06-21 21:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ english_count = 0
 mistakes = []
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-3.1-flash-lite")
 
 st.title("StudyMate AI")
